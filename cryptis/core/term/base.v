924d928fbc0b18f4026cdf00ef2c8cf753d5540a
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2025-09-25 15:20:53 -0400

    add TInv smart constructor

--- cryptis/core/term/base.v
+++ cryptis/core/term/base.v
@@ -154,6 +154,17 @@ Proof. exact: can_inj unfold_termK. Qed.
 
 Implicit Types (t k : term) (ts : seq term).
 
+Section TInv.
+
+Fact TInv_key : unit. Proof. exact: tt. Qed.
+Definition TInv :=
+  locked_with TInv_key (
+      fun t => fold_term (PreTerm.PT1 O1Inv (unfold_term t))
+  ).
+Canonical TInv_unlock := [unlockable of TInv].
+
+End TInv.
+
 Section TExp.
 
 Fact TExpN_key : unit. Proof. exact: tt. Qed.
@@ -205,6 +216,11 @@ Proof.
 rewrite map_id_in // => ? /mapP [] {}t t_ts ->.
 exact: normalize_unfold1.
 Qed.
+
+Lemma unfold_TInv t :
+  unfold_term (TInv t)
+  = PreTerm.inv (unfold_term t).
+Proof. by rewrite unlock unfold_fold /= normalize_unfold1. Qed.
 
 Lemma unfold_TExpN t ts :
   unfold_term (TExpN t ts)
