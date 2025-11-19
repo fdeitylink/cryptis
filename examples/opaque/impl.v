05407936f49a1da6912fd3b32d3554d4b4d076c9
Lyx <lyx@hustonfam.net>
2025-11-19 16:40:38 -0500

    structure /examples/opaque/impl.v after /examples/store/impl.v

--- examples/opaque/impl.v
+++ examples/opaque/impl.v
@@ -14,8 +14,6 @@ From cryptis.examples Require Import alist.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
-
-Section Opaque.
   
 Implicit Types N : namespace.
 
@@ -33,17 +31,9 @@ Definition KE : val := λ: "H" "p_a" "x_a" "P_b" "X_b",
 
 (* rw needs to be wrapped in derive_senc *)
 
-(* not useful: assume that files in db are properly formed instead *)
-(* but maybe use this as an example?  that the files can be computed. *)
-Definition make_file : val := λ: "g" "H" "H'" "AuthEnc" "pw",
-    let: "k_s" := mk_nonce #() in
-    let: "rw" := (OPRF "H" "H'" "k_s") "pw" in
-    let: "p_s" := mk_nonce #() in
-    let: "p_u" := mk_nonce #() in
-    let: "P_s" := texp "g" "p_s" in
-    let: "P_u" := texp "g" "p_u" in
-    let: "c" := "AuthEnc" "rw" ["p_u"; "P_u"; "P_s"] in
-    SOME ["k_s"; "p_s"; "P_s"; "P_u"; "c"].
+Module Client.
+
+Section Client.
 
 Definition user_session : val := λ: "g" "H'" "pw",
     let: "r" := mk_nonce #() in
@@ -51,20 +41,6 @@ Definition user_session : val := λ: "g" "H'" "pw",
     let: "α" := texp ("H'" "pw") in
     let: "X_u" := texp "g" "x_u" in
     SOME ["r"; "x_u"; (* pub-mark *) "α"; "X_u"].
-
-Definition server_session : val
-  := λ: "sid" "ssid" "g" "H" "db" "prf" "α" "X_u",
-    (* TODO: check α ∈ G *)
-    bind: "file" := AList.find "db" "sid" in
-      list_match: ["k_s"; "p_s"; "P_s"; "P_u"; "c"] := "file" in
-        let: "x_s" := mk_nonce #() in
-        let: "β" := texp "α" "k_s" in
-        let: "X_s" := texp "g" "x_s" in
-        let: "K" := "KE" "H" "p_s" "x_s" "P_u" "X_u" in
-        let: "ssid'" := "H" ["sid" "ssid" "α"] in
-        let: "SK" := "prf" ["K"; #0; "ssid'"] in
-        let: "A_s" := "prf" ["K"; #1; "ssid'"] in
-        SOME ["K"; "ssid'"; (* pub-mark *) "β"; "X_s"; "c"; "A_s"].
 
 Definition user_receive : val
   := λ: "sid" "ssid" "H" "AuthDec" "prf" "pw" "r" "β" "X_s" "c" "A_s",
@@ -79,7 +55,52 @@ Definition user_receive : val
           let: "A_u" := "prf" ["K"; #2; "ssid'"] in
           SOME ["sid"; "ssid"; "SK"; (* pub-mark *) "A_u"]
         else
-          NONE
+          NONE.
+
+(* TODO: do actual connections *)
+Definition user_flow : val
+  := λ: "sid" "ssid" "g" "H" "H'" "prf" "AuthEnc" "AuthDec" "pw",
+    list_match: ["r"; "x_u"; (* pub-mark *) "α"; "X_u"] := user_session "g" "H'" "pw" in
+      send-thing ["α"; "X_s"] ;;
+      list_match: ["β"; "X_s"; "c"; "A_s"] := receive-thing in
+        bind: "user_data" := user_receive "sid" "ssid" "H" "AuthDec" "prf" "pw" "r" "β" "X_s" "c" "A_s" in
+          list_match: ["sid"; "ssid"; "SK"; (* pub-mark *) "A_u"] := "user_data" in
+            send-thing "A_u" ;;
+            SOME ["sid"; "ssid"; "SK"].
+
+End Client.
+
+End Client.
+
+Module Server.
+
+Section Server.
+
+(* not useful: assume that files in db are properly formed instead *)
+(* but maybe use this as an example?  that the files can be computed. *)
+Definition make_file : val := λ: "g" "H" "H'" "AuthEnc" "pw",
+    let: "k_s" := mk_nonce #() in
+    let: "rw" := (OPRF "H" "H'" "k_s") "pw" in
+    let: "p_s" := mk_nonce #() in
+    let: "p_u" := mk_nonce #() in
+    let: "P_s" := texp "g" "p_s" in
+    let: "P_u" := texp "g" "p_u" in
+    let: "c" := "AuthEnc" "rw" ["p_u"; "P_u"; "P_s"] in
+    SOME ["k_s"; "p_s"; "P_s"; "P_u"; "c"].
+
+Definition server_session : val
+  := λ: "sid" "ssid" "g" "H" "db" "prf" "α" "X_u",
+    (* TODO: check α ∈ G *)
+    bind: "file" := AList.find "db" "sid" in
+      list_match: ["k_s"; "p_s"; "P_s"; "P_u"; "c"] := "file" in
+        let: "x_s" := mk_nonce #() in
+        let: "β" := texp "α" "k_s" in
+        let: "X_s" := texp "g" "x_s" in
+        let: "K" := "KE" "H" "p_s" "x_s" "P_u" "X_u" in
+        let: "ssid'" := "H" ["sid" "ssid" "α"] in
+        let: "SK" := "prf" ["K"; #0; "ssid'"] in
+        let: "A_s" := "prf" ["K"; #1; "ssid'"] in
+        SOME ["K"; "ssid'"; (* pub-mark *) "β"; "X_s"; "c"; "A_s"].
 
 Definition server_receive : val
   := λ: "sid" "ssid" "prf" "k" "ssid'" "SK" "A_u",
@@ -100,15 +121,6 @@ Definition server_flow : val
           let: "A_u" := receive-thing in
           server_receive "sid" "ssid" "prf" "k" "ssid'" "SK" "A_u".
 
-(* TODO: do actual connections *)
-Definition user_flow : val
-  := λ: "sid" "ssid" "g" "H" "H'" "prf" "AuthEnc" "AuthDec" "pw",
-    list_match: ["r"; "x_u"; (* pub-mark *) "α"; "X_u"] := user_session "g" "H'" "pw" in
-      send-thing ["α"; "X_s"] ;;
-      list_match: ["β"; "X_s"; "c"; "A_s"] := receive-thing in
-        bind: "user_data" := user_receive "sid" "ssid" "H" "AuthDec" "prf" "pw" "r" "β" "X_s" "c" "A_s" in
-          list_match: ["sid"; "ssid"; "SK"; (* pub-mark *) "A_u"] := "user_data" in
-            send-thing "A_u" ;;
-            SOME ["sid"; "ssid"; "SK"].
+End Server.
 
-End Opaque.
+End Server.
