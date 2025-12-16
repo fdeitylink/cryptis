002e88a301298980e219c428dadae9765984ecfc
Lyx <lyx@hustonfam.net>
2025-12-16 15:46:34 -0500

    cryptis: preliminary attempts to enforce structure of server db

--- examples/opaque/server_proofs.v
+++ examples/opaque/server_proofs.v
@@ -1,5 +1,7 @@
 From stdpp Require Import base gmap.
 From mathcomp Require Import ssreflect.
+(* From iris.algebra Require Import agree auth csum gset gmap excl frac. *)
+(* From iris.algebra Require Import max_prefix_list. *)
 From iris.heap_lang Require Import notation proofmode.
 From iris.heap_lang.lib Require Import par.
 From cryptis Require Import lib term cryptis primitives tactics.
@@ -7,23 +9,37 @@ From cryptis Require Import lib term cryptis primitives tactics.
 From cryptis.examples Require Import alist.
 From cryptis.examples.opaque Require Import impl shared.
 
+From iris.base_logic.lib Require Import invariants.
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Section Opaque.
 
-Context `{!cryptisGS Σ, !heapGS Σ, !spawnG Σ}.
+(* Context `{!cryptisGS Σ, !heapGS Σ, !spawnG Σ}. *)
+Context `{!cryptisGS Σ, !heapGS Σ, !iso_dhGS Σ, !storeGS Σ, !tlockG Σ}.
+
 Notation iProp := (iProp Σ).
+
+Definition opaque_db (db : gmap term term) : iProp :=
+[∗ map] (k : term) ↦ (file : term) ∈ db,
+    public k ∗ (∃ k_s p_s P_s P_u envelope,
+                   ⌜file = Spec.of_list [k_s; p_s; P_s; P_u; envelope]⌝
+                  ∗ minted k_s ∗ minted p_s ∗ public P_s ∗ public P_u ∗ public envelope).
+
+Lemma list_inv 
 
 Lemma wp_server_session (db c : term) (alist : gmap term term): 
 cryptis_ctx -∗
 channel c -∗
 AList.is_alist db (repr <$> alist) -∗
+opaque_db alist -∗
 WP Server.session db c
 {{ x , True }}.
 Proof.
-  iIntros "#Cryptis #Hc Hdb".
+  rewrite /opaque_db big_sepM_forall.
+  iIntros "#Cryptis #Hc Hdb #Hmapcontents".
   wp_lam. wp_pures.
   wp_apply (wp_recv with "Hc").
   iIntros "%m1 #Hpubm1".
@@ -38,15 +54,29 @@ Proof.
   iIntros "!> Hdb". rewrite lookup_fmap.
   case db_uid: (alist !! uid) => [file|]; wp_pures.
   2: by iModIntro.
-  do !rewrite subst_list_match /=.
-  clear db_uid.
-  wp_list_of_term_eq file' e.
-  2: wp_pures.
-  2: by iModIntro.
+  
+  iDestruct ("Hmapcontents" $! uid file with "[//]") as
+    "[_ (%k_s' & %p_s' & %P_s' & %P_u' & %envelope' &
+        %e' & Hmk_s & Hmp_s & HpP_s & HpP_u & Hpenvelope)]".
+
+  repeat rewrite subst_list_match /=.
+
+  
+wp_apply wp_list_of_term.
+rewrite e'.
+rewrite Spec.
+
+  
+2: wp_pures.
+  2: by iModIntro.  
   wp_pures.
-  wp_list_match => [k_s p_s P_s P_u envelope _ | ].
-  2: intro H; wp_pures.
-  2: by iModIntro.
+
+  unfold repr_list.
+
+  repeat rewrite subst_list_match /=.
+  
+wp_list_match => [k_s p_s P_s P_u envelope _ | ].
+  2: intro H; wp_pures; by iModIntro.
   wp_apply (wp_mk_nonce (fun _ => False)%I (fun _ => False)%I).
   1: iAssumption.
   iIntros "%x_s %Hnoncex_s #Hmintedx_s #Hprivatex_s #H!eqx_s Htokenx_s".
@@ -77,13 +107,15 @@ Proof.
   wp_term_of_list.
   wp_pures.
   rewrite Hm1eq.
-  rewrite public_of_list /=.
+  rewrite public_of_list.
+  simpl.
   iDestruct "Hpubm1" as "[Hpubuid [Hpubα [HpubX_u _]]]".
   wp_apply wp_send.
   1: done.
-  1: do !rewrite public_of_list /=.
+  1: rewrite public_of_list => //.
   do !iSplit => //.
-  iApply public_TExp => //.
+  iApply public_TExp_iff => //.
+  admit.
   admit.
   iApply public_TExp_iff.
   intro contra. destruct contra.
@@ -93,6 +125,18 @@ Proof.
   iApply "H!eqx_s". iNext. iModIntro. admit.
   admit.
   iApply public_THash.
+  iRight. iSplit.
+  iApply minted_tag. iApply minted_of_list. do !iSplit => //.
+  iApply minted_THash. iApply minted_tag. iApply minted_of_list. do !iSplit => //.
+  iApply minted_TExp.
+  admit.
+  admit.
+  iApply minted_TExp.
+  admit.
+  admit.
+  iApply minted_THash. iApply minted_tag. iApply minted_of_list. do !iSplit => //.
+  admit.
+  admit.
   admit.
   wp_pures.
   wp_apply (wp_recv with "Hc").
